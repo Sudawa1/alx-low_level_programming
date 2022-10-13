1b4944b8d1092dfc0dc1a551234383851f69af30
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-13 06:46:45 -0700

    hhhhh

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h> 
- /** 
-  * main - entry point 
-  * 
-  * Return: Always 0 
-  */ 
- int main(void) 
- { 
- puts("\"Programming is like building a multilingual puzzle"); 
- return (0); 
- }
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+puts(:"\"programming is like building a multilingualpuzzle");
+return (0);
+}
