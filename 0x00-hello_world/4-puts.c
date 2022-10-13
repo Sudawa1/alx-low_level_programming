61fc5d2242b7aab87438347a6644a453d3404158
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-13 16:18:44 -0700

    bjjjjjjjjj# Please enter the commit message for your changes. Lines starting

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
- #include < stdio.h>
+#include <stdio.h>
 /**
- * main - entry point
- *
- * Return: Always 0
+ * main - Entry point
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-puts(:"\"programming is like building a multilingualpuzzle");
-return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+
+		return (0);
 }
