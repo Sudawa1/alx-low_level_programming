bdac274c7872a2269cfa70877a9980a3bda198fb
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-13 16:03:52 -0700

    ffffffr

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,7 @@
-#include <stdio.h> 
- /** 
-  * main - Entry point 
-  * 
-  * Return: 0 
-  */ 
- int main(void) 
- { 
-         printf("with proper grammar, but the outcome is a piece of art,\n"); 
-         return (0); 
- }
+include <stdio.h>
+ 4
+ 5   // function main begins program execution
+ 6   int main( void )
+ 7   {
+ 8      printf( "with proper grammar, but the outcome is a piece of art\n" );
+ 9   } // end function main
