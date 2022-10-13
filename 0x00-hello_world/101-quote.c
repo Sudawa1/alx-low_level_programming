67598eeb0745df1dbe5039562639fac6892ce92c
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-13 15:07:57 -0700

    ahhghdhhhhj

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,25 +1,13 @@
 #include <stdio.h> 
-<<<<<<< HEAD
+ #include <unistd.h> 
   
  /** 
-=======
-** 
->>>>>>> f0f86ef0147ff2fbda99eabc33f845b0f3aaba05
   * main - Entry point 
   * 
   * Return: Always 0 (Success) 
   */ 
-<<<<<<< HEAD
  int main(void) 
  { 
-         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
-  
+         write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59); 
          return (1); 
- }        
-=======
-int main(void) 
-{ 
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61); 
-return (1); 
-}
->>>>>>> f0f86ef0147ff2fbda99eabc33f845b0f3aaba05
+ }
