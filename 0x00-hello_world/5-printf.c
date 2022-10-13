d6f75c17470299af561a6d70bb7e39fbc4df4418
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-13 16:06:53 -0700

    bjjjjjjjjjj# Please enter the commit message for your changes. Lines starting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
-include <stdio.h>
- 4
- 5   // function main begins program execution
- 6   int main( void )
- 7   {
- 8      printf( "with proper grammar, but the outcome is a piece of art\n" );
- 9   } // end function main
+#include <stdio.h> 
+  
+ /** 
+  * main - Entry point 
+  * 
+  * Return: Always 0 (Success) 
+  */ 
+ int main(void) 
+ { 
+ printf("with proper grammar, but the outcome is a piece of art,\n"); 
+ return (0);
