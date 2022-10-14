85482c95af95997eeb5a62d00635fc6e3efc7c4c
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-14 09:03:12 -0700

    aliyu

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
 int main(void)
-{int integerType;
-float floatType;
-double doubleType;
-char Type;
-
-Size of operator is used to evaluate the size of a variable
-printf("Size of int: %ld bytes\n", size of(integerType));
-printf("Size of float: %ld bytes\n", size of(floatType));
-printf("Size of double: %ld bytes\n", size of(doubleType));
-printf("Size of char: %ld byte\n", size of(charType));
-
+{printf("size of a char: %i byte(s)\n", sizeof(char));
+printf("Size of an int: %i byte(s)\n", sizeof(int));
+printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+printf("Size of a long long int: %i byte(s)\n", sizeof(long long int));
+printf("Size of a float: %i byte(s)\n", sizeof(float))
 return (0);
 }
