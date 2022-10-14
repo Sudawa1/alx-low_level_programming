f19dd0976cce692ea2cfdc96290e34ab090c6917
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-14 03:22:10 -0700

    what i didi	# Please enter the commit message for your changes. Lines starting

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,12 +1,14 @@
-#include <unistd.h>
+#include <stdio.h>
+/**
+ * main - Entry - point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	printf("
+			with a proper grammar, but the outcome is a peice of art,\n"
+	      );
+return(0);
+}
 
- /**
-  * main - Entry point
-  *
-  * Return: Always 0 (Success)
-  */
- int main(void)
- {
-         printf ("with proper grammar, but the outcome is a piece of art,\n");
-         return (0);
- }
