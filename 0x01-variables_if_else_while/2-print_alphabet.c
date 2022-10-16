13d228e9191fd410babefb4e4a27f4e1b204a128
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-16 14:45:04 -0700

    ality

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,33 +1,15 @@
 #include <stdio.h>
-
-void main()
-
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
 {
-
-        int ch;
-
-
-        clrscr();
-
-        printf(“Print all uppercase letters followed by all lowercase letters on the next line :\n”);
-
-        /* print uppercase letters */
-
-
-        for (ch= ‘A’; ch<= ‘Z’; ch++)
-
-             putchar(ch);
-
-             putchar (‘\n’ ) ;
-
-             /* print lowercase letters */
-
-             for (ch= ‘a’; ch<= ‘z’; ch++)
-
-                  putchar(ch);
-
-                  putchar (‘\n’) ;
-
-                  getch();
-
+char low;
+for (low = 'a'; low <= 'z'; low++)
+putchar(low);
+putchar ('\n');
+return (0);
 }
