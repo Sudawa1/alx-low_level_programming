b3b6bdba89d7110d717fa81c2cc2108ec7aac2ea
Aliyu isah aliyu <Aliyuisahaliyu70@gmail.com>
2022-10-16 04:09:52 -0700

    aliyu

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,12 +1,14 @@
-#include<stdlib.h>
-#include<time.h>
-/* more headers goes there */ 
-/* betty style doc for function main goes there */ 
+#include <stdlib.h>
+#include <time.h>
+/* more headers go there */
+
+/* betty style doc for function main goes there */
 int main(void)
-{
-int n;
-srand(time(0));
-n = rand() - RAND_MAX / 2;
-/* your code goes there */
-return (0);
+{ 
+	int n;
+
+	srand(time(0));
+	n=rand() - RAND_MAX / 2;
+	/* your codes go there */
+	return(0);
 }
