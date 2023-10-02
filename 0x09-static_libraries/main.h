e853bd63efac48653cfb020938d3b53cb6049152
aliyu isah aliyu <aliyuisahaliyu70@gmail.com>
2023-10-02 12:08:07 -0700

    sliyu

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -22,4 +22,114 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
 
-#endif
+
+We're moving to Discord!
+In a few days, we will be leaving Slack in favor of Discord 🎉
+Click here for more information
+Signed in successfully.
+0x09. C - Static libraries
+C
+ By: Julien Barbier
+ Weight: 1
+ Project will start Oct 2, 2023 6:00 AM, must end by Oct 3, 2023 6:00 AM
+ Checker was released at Oct 2, 2023 12:00 PM
+ An auto review will be launched at the deadline
+Concepts
+For this project, we expect you to look at this concept:
+
+C static libraries
+Resources
+Read or watch:
+
+What Is A “C” Library? What Is It Good For?
+Creating A Static “C” Library Using “ar” and “ranlib”
+Using A “C” Library In A Program
+What is difference between Dynamic and Static library(Static and Dynamic linking) (stop at 4:44)
+man or help:
+
+ar
+ranlib
+nm
+Learning Objectives
+At the end of this project, you are expected to be able to explain to anyone, without the help of Google:
+
+General
+What is a static library, how does it work, how to create one, and how to use it
+Basic usage of ar, ranlib, nm
+Copyright - Plagiarism
+You are tasked to come up with solutions for the tasks below yourself to meet with the above learning objectives.
+You will not be able to meet the objectives of this or any following project by copying and pasting someone else’s work.
+You are not allowed to publish any content of this project.
+Any form of plagiarism is strictly forbidden and will result in removal from the program.
+Requirements
+C
+Allowed editors: vi, vim, emacs
+All your files will be compiled on Ubuntu 20.04 LTS using gcc, using the options -Wall -Werror -Wextra -pedantic -std=gnu89
+All your files should end with a new line
+A README.md file, at the root of the folder of the project is mandatory
+Your code should use the Betty style. It will be checked using betty-style.pl and betty-doc.pl
+You are not allowed to use global variables
+No more than 5 functions per file
+You are not allowed to use the standard library. Any use of functions like printf, puts, etc… is forbidden
+You are allowed to use _putchar
+You don’t have to push _putchar.c, we will use our file. If you do it won’t be taken into account
+In the following examples, the main.c files are shown as examples. You can use them to test your functions, but you don’t have to push them to your repo (if you do we won’t take them into account). We will use our own main.c files at compilation. Our main.c files might be different from the one shown in the examples
+The prototypes of all your functions and the prototype of the function _putchar should be included in your header file called main.h
+Don’t forget to push your header file
+Bash
+Allowed editors: vi, vim, emacs
+All your scripts will be tested on Ubuntu 20.04 LTS
+All your files should end with a new line (why?)
+The first line of all your files should be exactly #!/bin/bash
+A README.md file, at the root of the folder of the project, describing what each script is doing
+All your files must be executable
+More Info
+You do not need to learn about dynamic libraries, yet.
+
+Quiz questions
+Great! You've completed the quiz successfully! Keep going! (Show quiz)
+Tasks
+0. A library is not a luxury but one of the necessities of life
+mandatory
+Create the static library libmy.a containing all the functions listed below:
+
+int _putchar(char c);
+int _islower(int c);
+int _isalpha(int c);
+int _abs(int n);
+int _isupper(int c);
+int _isdigit(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
+char *_strcat(char *dest, char *src);
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
+int _strcmp(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
+char *_memcpy(char *dest, char *src, unsigned int n);
+char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
+char *_strpbrk(char *s, char *accept);
+char *_strstr(char *haystack, char *needle);
+0-isupper.o
+0-memset.o
+0-strcat.o
+1-isdigit.o
+1-memcpy.o
+1-strncat.o
+100-atoi.o
+2-strchr.o
+2-strlen.o
+2-strncpy.o
+3-islower.o
+3-puts.o
+3-strcmp.o
+3-strspn.o
+4-isalpha.o
+4-strpbrk.o
+5-strstr.o
+6-abs.o
+9-strcpy.o
+_putchar.o
