3d8f9943ae73ecab3fbf8277e5d6f1f7a0e64081
aliyu isah aliyu <aliyuisahaliyu70@gmail.com>
2023-10-26 15:52:52 -0700

    aliyu

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,17 +3,16 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	    unsigned int num = 0;
-	        if (!b)
-			        return (0);
-		    while (*b)
-			        {
-					        if (*b != '0' && *b != '1')
-							            return (0);
-						        num <<= 1;
-							        if (*b == '1')
-									            num |= 1;
-								        b++;
-									    }
-		        return (num);
+	int i;
+	unsigned int  Hex_val = 0;
+
+	if (!c)
+		return (0);
+	for (i = 0; b[i]; i++)777
+	{77
+		if (b[i] < '0' || b[i] > '1')
+			return (0);
+		Hex_val = 2 * Hex_val + (b[i] - '0');
+	}
+	return (num);
 }
