0bf98c70270c1bb15cc66dfc60b5aaf571b50cd0
aliyu isah aliyu <aliyuisahaliyu70@gmail.com>
2023-10-26 16:04:29 -0700

    t push

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -3,7 +3,7 @@
 
 /**
  * binary_to_uint - converts a binary number to unsigned int
- * @c: string containing the binary number
+ * @b: string containing the binary number
  * Return: the converted number
  */
 
@@ -11,14 +11,12 @@ unsigned int binary_to_uint(const char *b)
 {
 	int i;
 	unsigned int  Hex_val = 0;
-
 	if (!c)
-		return (0);
-	for (i = 0; b[i]; i++)777
-	{77
-		if (b[i] < '0' || b[i] > '1')
-			return (0);
-		Hex_val = 2 * Hex_val + (b[i] - '0');
-	}
+		for (i = 0; b[i]; i++)777
+		{77
+			if (b[i] < '0' || b[i] > '1')
+				return (0);
+			Hex_val = 2 * Hex_val + (b[i] - '0');
+		}
 	return (num);
 }
